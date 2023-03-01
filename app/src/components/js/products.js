978c23a5710b68187b4191ab3fab3bdd8e8ad79a
JoseHS06 <armando.hernandez@vlim.com.mx>
2023-02-28 23:09:32 -0600

    Update

---
+++ app/src/components/js/products.js
@@ -0,0 +1,21 @@
+const productList = [
+    {
+      name: "Laptop Notebook HP",
+      stock: 20,
+    },
+  
+    {
+      name: "Mouse HP",
+      stock: 30,
+    },
+    {
+      name: "Mouse HP",
+      stock: 30,
+    },
+    {
+      name: "Mouse HP",
+      stock: 30,
+    },
+  ];
+  
+  export { productList };
