9b195f574b7bf0fafd9f3b58213515e8e7b58835
José Armando Hernández Salamanca <armando.hernandez@vlim.com.mx>
2023-03-03 17:00:12 -0600

    Update

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import express, { query } from "express";
-import {cnn} from './db/db.js';
+import { conexion } from './db/db.js';
 
 const app = express();
 app.listen(3000);
@@ -7,9 +7,50 @@ app.listen(3000);
 
 app.get('/addProduct', async (req, res) => {
 
-    const result = await cnn.query('SELECT 1 + 1 AS result');
-    res.json(result[0]);
+    try {
 
+        const { code, name, stock } = req.body;
+        const [rows] = await conexion.query('', [code, name, stock]);
+
+        return res.status(200).json({ message: 'Producto agregado correctamente ' });
+
+    } catch (error) {
+
+        return res.status(300).json({ message: 'Ocurrió un error al agregar al producto' });
+
+    }
+});
+
+app.get('/addStock', async (req, res) => {
+
+    try {
+
+        const { id, stock } = req.body;
+        const [rows] = await conexion.query('', [id, stock]);
+
+        return res.status(200).json({ message: 'Stock actualizado correctamente ' });
+
+    } catch (error) {
+
+        return res.status(300).json({ message: 'Ocurrió un error al agregar el stock' });
+
+    }
+});
+
+app.get('/removeStock', async (req, res) => {
+
+    try {
+
+        const { id, stock } = req.body;
+        const [rows] = await conexion.query('', [id, stock]);
+
+        return res.status(200).json({ message: 'Stock actualizado correctamente ' });
+
+    } catch (error) {
+
+        return res.status(300).json({ message: 'Ocurrió un error al eliminar el stock' });
+
+    }
 });
 
 
