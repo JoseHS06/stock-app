d4fbc753aa16e22dad56a4741e579106cedb1fad
José Armando Hernández Salamanca <armando.hernandez@vlim.com.mx>
2023-03-14 09:37:47 -0600

    Update

--- app/src/app.js
+++ app/src/app.js
@@ -1,5 +1,4 @@
 const getProducts = async () => {
-<<<<<<< Updated upstream
   try {
     return new Promise((resolve, reject) => {
       fetch("http://localhost:3000/getLog/0", {
@@ -39,48 +38,6 @@ const getInventary = async () => {
 
 
 
-=======
-    try {
-
-        return new Promise((resolve, reject) =>{
-            fetch("http://localhost:3000/getLog/0", {
-            method: "GET",
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-                "Access-Control-Allow-Origin": '*',
-            }
-           }).then((response) => response.json())
-              .then((resolve))
-              .catch((reject));
-        });
-
-    } catch (error) {
-        console.warn('Ocurrió un error al obtener los productos' + error);
-    }
-};
-
-
-const getInventary = async () => {
-    try {
-
-        return new Promise((resolve, reject) =>{
-            fetch("http://localhost:3000/getLog/1", {
-            method: "GET",
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-                "Access-Control-Allow-Origin": '*',
-            }
-           }).then((response) => response.json())
-              .then((resolve))
-              .catch((reject));
-        });
-
-    } catch (error) {
-        console.warn('Ocurrió un error al obtener los productos ' + error);
-    }
-};
-
->>>>>>> Stashed changes
 const addProduct = async (product) => {
   try {
     const { code, name, stock, stock_minimum } = product;
@@ -100,7 +57,6 @@ const addProduct = async (product) => {
 
     const { status } = response;
 
-<<<<<<< Updated upstream
     if (status == 200) {
       return {
         status,
@@ -110,20 +66,6 @@ const addProduct = async (product) => {
   } catch (error) {
     console.warn("Ocurrió un error al obtener el inventario " + error);
   }
-=======
-        if(status == 200){
-            
-            return {
-                status,
-                message: 'Producto agregado con éxito'
-            };
-        }
-
-        
-    } catch (error) {
-        console.warn('Ocurrió un error al registrar el producto ' + error);
-     }
->>>>>>> Stashed changes
 };
 
 const updateStock = async (action, id, stock) => {
@@ -154,10 +96,4 @@ const updateStock = async (action, id, stock) => {
   }
 };
 
-<<<<<<< Updated upstream
 export { getProducts, getInventary, addProduct, updateStock };
-=======
-
-
-export { getProducts, getInventary, addProduct, updateStock}
->>>>>>> Stashed changes
